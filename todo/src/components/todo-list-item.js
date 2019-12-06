db74c1a7a87f83ea40938725c080dd755a5a5b37
Roman Golubev <gromniki@gmail.com>
2019-12-06 18:13:52 +0300

    props

--- todo/src/components/todo-list-item.js
+++ todo/src/components/todo-list-item.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const TodoListItem = ( { label } ) => {
-  const liStyle = {
+const TodoListItem = ({label, important = false}) => {
+  const style = {
     color: important ? 'tomato' : 'black'
   };
 
-  return <span>{ label }</span>;
+  return <span style={style}>{label}</span>;
 };
 
 export default TodoListItem;
@@ -23,3 +23,4 @@ export default TodoListItem;
  * };
  *
  * */
+
